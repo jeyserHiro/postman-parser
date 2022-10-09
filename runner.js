59fc31a563caa678dd1739a684954430e1244c31
Hiro <109828238+jeyserHiro@users.noreply.github.com>
2022-10-09 00:57:33 +0000

    bugs fixed.......

--- runner.js
+++ runner.js
@@ -3,31 +3,34 @@ import fs from 'fs'
 
 import { SocksProxyAgent } from 'socks-proxy-agent';
 
-const info = {
-	hostname: 'br41.nordvpn.com',
-	userId: 'your-name@gmail.com',
-	password: 'abcdef12345124'
-};
-const agent = new SocksProxyAgent(info);
+// const info = {
+// 	hostname: 'br41.nordvpn.com',
+// 	userId: 'your-name@gmail.com',
+// 	password: 'abcdef12345124'
+// };
+// const agent = new SocksProxyAgent(info);
 
-const requestAgent = new SocksProxyAgent({ hostname: 'localhost', port: '4003'});
+const requestAgent = new SocksProxyAgent({ hostname: 'localhost', port: '4003' });
 
 let filename = 'sample-test.collection.json'
 filename = 'Hypertest-Application.postman_collection.json'
+filename = 'v2.1.json'
+filename = 'testv2.1.json'
 
-let file1 = fs.readFileSync(filename)
 
-console.debug("going to run.....")
-newman.run({
-  collection: file1,
-  reporters: 'cli', //['htmlextra', 'csv','cli'],
-  iterationCount:10,
-  color:true,
-  requestAgents: {
-        http: requestAgent, // agent used for HTTP requests
-        https: requestAgent, // agent used for HTTPS requests
-  }
-}, (err) => {
-  if (err) { throw err; }
-  console.log('collection run complete!');
-})
+    
+while (true) {
+  newman.run({
+    collection: filename,
+    reporters: 'cli', //['htmlextra', 'csv','cli'],
+    iterationCount: 10,
+    color: true, 
+    requestAgents: {
+      http: requestAgent, // agent used for HTTP requests
+      https: requestAgent, // agent used for HTTPS requests
+    }
+  }, (err) => {
+    if (err) { throw err; }
+    console.log('collection run complete!')
+  })
+}
